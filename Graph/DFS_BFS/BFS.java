a6cd35ac7c7ae1e7f3bd05afb1e7643e7fe39e61
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-10-24 09:12:54 +0530

    DFS / BFS
    DFS problem 1

---
+++ Graph/DFS_BFS/BFS.java
@@ -0,0 +1,70 @@
+package Graph.DFS_BFS;
+import java.util.*;
+
+class BFS{
+    public static void main(String[] args) {
+        ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
+        for (int i = 0; i < 5; i++) {
+            adj.add(new ArrayList < > ());
+        }
+        adj.get(0).add(2);
+        adj.get(2).add(0);
+        adj.get(0).add(1);
+        adj.get(1).add(0);
+        adj.get(0).add(3);
+        adj.get(3).add(0);
+        adj.get(2).add(4);
+        adj.get(4).add(2);
+
+
+       // ArrayList < Integer > ans = bfsOfGraph(5, adj);
+        ArrayList < Integer > ans = dfsOfGraph(5, adj);
+        int n = ans.size();
+        for(int i = 0;i<n;i++) {
+            System.out.print(ans.get(i)+" ");
+        }
+
+    }
+    // Function to return Breadth First Traversal of given graph.
+    public static ArrayList<Integer> bfsOfGraph(int V,
+                                         ArrayList<ArrayList<Integer>> adj) {
+        Queue<Integer> q=new LinkedList<>();
+        boolean visited[]=new boolean[V+1];
+        ArrayList<Integer> bfs=new ArrayList<Integer>();
+        q.add(0);
+        visited[0]=true;
+        while(!q.isEmpty()) {
+            Integer node =q.poll();
+            bfs.add(node);
+            for (Integer it : adj.get(node)) {
+                if (!visited[it]) {
+                    visited[it]=true;
+                    q.add(it);
+                }
+            }
+
+        }
+        return  bfs;
+    }
+
+    // Function to return a list containing the DFS traversal of the graph.
+    public static ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean visited[]=new boolean[V+1];
+        //visited[0]=true;
+        ArrayList<Integer> ls = new ArrayList<>();
+        dfs(0,visited,adj,ls);
+        return ls;
+
+    }
+    public static void dfs(int node, boolean vis[], ArrayList<ArrayList<Integer>> adj,
+                           ArrayList<Integer> ls) {
+        vis[node]=true;
+        ls.add(node);
+        for(Integer it :adj.get(node)){
+            if (!vis[it]){
+                dfs(it,vis,adj,ls);
+            }
+        }
+
+    }
+}
---
+++ Graph/DFS_BFS/EvaluateDivision_399.java
@@ -0,0 +1,69 @@
+package Graph.DFS_BFS;
+import java.util.*;
+
+/*
+https://leetcode.com/problems/evaluate-division/
+ */
+public class EvaluateDivision_399 {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries)
+    {
+        HashMap<String,HashMap<String,Double>> gp=buildGraph(equations,values);
+        double[] finalAns = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+            String dividend = queries.get(i).get(0);
+            String divisor = queries.get(i).get(1);
+
+            if( !gp.containsKey(dividend) || !gp.containsKey(divisor)){
+                finalAns[i]=-1.0;
+            }
+            else{
+                HashSet<String> vis = new HashSet<>();
+                double[] ans = {-1.0};
+                double temp = 1.0;
+                dfs(dividend, divisor, gp, vis, ans, temp);
+                finalAns[i] = ans[0];
+            }
+        }
+        return finalAns;
+
+
+    }
+    public void dfs(String node, String dest, HashMap<String, HashMap<String, Double>> gr, HashSet<String> vis, double[] ans, double temp) {
+        if (vis.contains(node))
+            return;
+        vis.add(node);
+        if (node.equals(dest))
+        {
+            ans[0]=temp;
+            return;
+        }
+        for (Map.Entry<String, Double> entry : gr.get(node).entrySet()) {
+            String ne = entry.getKey();
+            double val = entry.getValue();
+            dfs(ne, dest, gr, vis, ans, temp * val);
+        }
+
+    }
+
+
+    public HashMap<String, HashMap<String, Double>> buildGraph(List<List<String>> equations, double[] values) {
+        HashMap<String, HashMap<String, Double>> gr = new HashMap<>();
+
+        for(int i=0;i<equations.size();i++)
+        {
+            String x=equations.get(i).get(0);
+            String y=equations.get(i).get(1);
+            double weight=values[i];
+
+            gr.putIfAbsent(x,new HashMap<>());
+            gr.putIfAbsent(y,new HashMap<>());
+
+            gr.get(x).put(y,weight);
+            gr.get(y).put(x,1.0/weight);
+
+
+        }
+
+        return gr;
+    }
+}
