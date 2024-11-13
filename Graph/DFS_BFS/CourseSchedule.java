ca658f3762900dda8c911f3d4d97c7e033154ac8
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-11-13 15:20:23 +0530

    cycle detection

---
+++ Graph/DFS_BFS/CourseSchedule.java
@@ -0,0 +1,11 @@
+package Graph.DFS_BFS;
+
+public class CourseSchedule {
+    public static void main(String[] args) {
+        int numCourses = 4;
+        int [][] prerequisites = {{1,0},{2,0},{3,1},{3,2}};
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+return new int[]{};
+    }
+}
