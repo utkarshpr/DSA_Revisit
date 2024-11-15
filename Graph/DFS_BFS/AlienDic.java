9e51f6ebbd37a35b13aa9fdc8175130a6acbef5c
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-11-15 10:13:21 +0530

    added Alien dictionary ,course Schedule 1,2

---
+++ Graph/DFS_BFS/AlienDic.java
@@ -0,0 +1,73 @@
+package Graph.DFS_BFS;
+
+import java.util.ArrayList;
+import java.util.*;
+
+public class AlienDic {
+    public static void main(String[] args) {
+        int N = 5, K = 4;
+        String[] dict = {"baa", "abcd", "abca", "cab", "cad"};
+        //Solution obj = new Solution();
+        String ans = objfindOrder(dict, N, K);
+
+        for (int i = 0; i < ans.length(); i++) {
+            System.out.print(ans.charAt(i) + " ");
+        }
+        System.out.println("");
+    }
+
+    private static String objfindOrder(String[] dict, int n, int k) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n-1;i++){
+            String s1=dict[i];
+            String s2=dict[i+1];
+            int len=Math.min(s1.length(),s2.length());
+            for(int j=0;j<len;j++){
+                if(s1.charAt(j)!=s2.charAt(j)){
+                    adj.get(s1.charAt(j)-'a').add(s2.charAt(j)-'a');
+                    break;
+                }
+            }
+        }
+        List<Integer> topo = topoSorta(k, adj);
+        String ans = "";
+        for (int it : topo) {
+            ans = ans + (char)(it + (int)('a'));
+        }
+
+        return ans;
+
+    }
+
+    private static List<Integer> topoSorta(int V, List<List<Integer>> adj) {
+        int indegree[] = new int[V];
+        for (int i = 0; i < V; i++) {
+            for (int it : adj.get(i)) {
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        List<Integer> topo = new ArrayList<>();
+        while (!q.isEmpty()) {
+            int node = q.peek();
+            q.remove();
+            topo.add(node);
+            for (int it : adj.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0) q.add(it);
+            }
+        }
+
+        return topo;
+    }
+}
