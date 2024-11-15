aa9c7f3731fd686a2ad1f6688287b90daa65d767
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2024-11-15 09:36:25 +0530

    khan algo topo sort using indegree

---
+++ Graph/DFS_BFS/khan_TOPOSORT.java
@@ -0,0 +1,59 @@
+package Graph.DFS_BFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class khan_TOPOSORT {
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        adj.get(2).add(3);
+        adj.get(3).add(1);
+        adj.get(4).add(0);
+        adj.get(4).add(1);
+        adj.get(5).add(0);
+        adj.get(5).add(2);
+
+        int[] ans = StopoSort(V, adj);
+        for (int node : ans) {
+            System.out.print(node + " ");
+        }
+        System.out.println("");
+    }
+
+    private static int[] StopoSort(int v, ArrayList<ArrayList<Integer>> adj) {
+        int ind[]=new int[v];
+        for(int i=0;i<v;i++){
+            for(int it:adj.get(i)){
+                ind[it]++;
+            }
+        }
+        Stack<Integer> st=new Stack<>();
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<v;i++)
+        {
+            if (ind[i]==0)
+                q.add(i);
+        }
+        int top[]=new int[v];
+        int x=0;
+        while (!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+
+            top[x++]=node;
+            for (int it : adj.get(node)) {
+                ind[it]--;
+                if (ind[it] == 0)
+                    q.add(it);
+
+            }
+        }
+    return top;
+    }
+}
